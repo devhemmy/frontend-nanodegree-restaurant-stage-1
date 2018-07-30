9639e858ad53a6914f5d68843604ddfdcb03d389
Hemmy <hema.kappa55@gmail.com>
2018-07-30 09:43:29 +0200

    aria updates

--- js/main.js
+++ js/main.js
@@ -164,7 +164,7 @@ createRestaurantHTML = (restaurant) => {
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
@@ -178,6 +178,8 @@ createRestaurantHTML = (restaurant) => {
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
+  more.setAttribute('aria-label', 'View Details of ' +restaurant.name+ "'s Restaurant'" );
+  console.log('Hello again');
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
 
